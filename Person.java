96e693968c269f1e28630f472dd7896728b78553
FerencOnodi <onodiferenc91@gmail.com>
2017-02-23 10:54:49 +0100

    Implemented Person.java

---
+++ Person.java
@@ -0,0 +1,51 @@
+/**
+ * Created by ferenc on 2017.02.23..
+ */
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phone;
+
+    public Person(String firstName, String lastName, String address, String city, String state, String zip, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
